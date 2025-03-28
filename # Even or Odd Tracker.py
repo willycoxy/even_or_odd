03667ddfdafe788fe11534487f9d1e5304767b2b
William Cox <105467011+willycoxy@users.noreply.github.com>
2025-03-27 22:02:36 -0400

    Add files via upload

---
+++ # Even or Odd Tracker.py
@@ -0,0 +1,11 @@
+# Even or Odd Tracker
+
+# User inputs number
+number = int(input("Please enter a number. "))
+
+# if it can be % by 2 then its even, else its odd!
+
+if (number % 2 == 0):
+    print("Your number is Even!")
+else:
+    print("Your number is Odd!")
